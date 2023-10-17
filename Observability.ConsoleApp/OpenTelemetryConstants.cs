e3d01381e095620a6f3f32813f7e3620b6b78e68
Selçuk ARSLAN <slckarslan93@gmail.com>
2023-10-17 22:45:50 +0300

    OpenTelemetryConstants

---
+++ Observability.ConsoleApp/OpenTelemetryConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observability.ConsoleApp
+{
+    public class OpenTelemetryConstants
+    {
+        public const string ServiceName = "ConsoleApp";
+        public const string ServiceVersion = "1.0.0";
+    }
+}
--- Observability.ConsoleApp/Program.cs
+++ Observability.ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Observability.ConsoleApp;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 Console.WriteLine("Hello, World!");
+
+var traceProvider = Sdk.CreateTracerProviderBuilder()
+    .ConfigureResource(configure =>
+    {
+        configure.AddService(OpenTelemetryConstants.ServiceName, OpenTelemetryConstants.ServiceVersion)
+        .AddAttributes(new List<KeyValuePair<string, object>>()
+        {
+            new KeyValuePair<string,object>("host.machineName",Environment.MachineName),
+            new KeyValuePair<string,object>("host.environment","dev"),
+        });
+    }).Build();
