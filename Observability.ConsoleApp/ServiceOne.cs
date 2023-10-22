36010afdf8277ce70d8d42eb795248a4ad3f3cea
Selçuk ARSLAN <slckarslan93@gmail.com>
2023-10-22 12:24:45 +0300

    span

--- Observability.ConsoleApp/ServiceOne.cs
+++ Observability.ConsoleApp/ServiceOne.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,29 @@ namespace Observability.ConsoleApp
         public async Task<int> MakeRequestToGoogle()
         {
 
-            using var activity = ActivitySourceProvider.Source.StartActivity();
+            using var activity = ActivitySourceProvider.Source.StartActivity(kind:System.Diagnostics.ActivityKind.Producer,name: "CustomMakeRequestToGoogle");
             {
+                try
+                {
+                    var eventTags = new ActivityTagsCollection();
 
-                var result = await httpClient.GetAsync("http://www.goggle.com");
 
-                var responseContent = await result.Content.ReadAsStringAsync();
+                    activity?.AddEvent(new("google'a şstek başladı", tags: eventTags));
 
-                return responseContent.Length;
+                    var result = await httpClient.GetAsync("http://www.goggle.com");
+
+                    var responseContent = await result.Content.ReadAsStringAsync();
+
+                    eventTags.Add("google body length", responseContent.Length);
+                    activity?.AddEvent(new("google'a istek tamamlandı", tags: eventTags));
+                    return responseContent.Length;
+                }
+                catch (Exception ex)
+                {
+
+                    activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
+                    return -1;
+                }
 
             }
 
