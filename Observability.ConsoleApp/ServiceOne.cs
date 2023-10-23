0a0e3a28a0c0c81c79941f9466050f1216e6b6d4
Selçuk ARSLAN <slckarslan93@gmail.com>
2023-10-23 21:31:31 +0300

    Updated

--- Observability.ConsoleApp/ServiceOne.cs
+++ Observability.ConsoleApp/ServiceOne.cs
@@ -32,6 +32,11 @@ namespace Observability.ConsoleApp
 
                     eventTags.Add("google body length", responseContent.Length);
                     activity?.AddEvent(new("google'a istek tamamlandı", tags: eventTags));
+
+                    var serviceTwo = new ServiceTwo();
+
+                    var fileLenght = serviceTwo.WriteToFile("Merhaba Dünya");
+
                     return responseContent.Length;
                 }
                 catch (Exception ex)
---
+++ Observability.ConsoleApp/ServiceTwo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observability.ConsoleApp
+{
+    public class ServiceTwo
+    {
+        public async Task<int> WriteToFile(string text)
+        {
+            using (var activty = ActivitySourceProvider.Source.StartActivity())
+            {
+                await File.WriteAllTextAsync("myFile.txt", text);
+
+                return (await File.ReadAllTextAsync("myFile.txt")).Length;
+            }
+
+
+        }
+    }
+}
